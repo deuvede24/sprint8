ee2296257f717b8eac5154234b77d9ce71c82840
deuvede24 <cdaniela.vd@gmail.com>
2024-09-02 17:24:51 +0200

    navbar updated

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -14,6 +14,9 @@ export class NavbarComponent {
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+
 
 @Component({
   selector: 'app-navbar',
@@ -22,5 +25,9 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(public authService: AuthService) {}
+}
+//authService: any;
+
 
