e1169a4bc1c4290462449d6aeb220a2dd9aa8ed7
deuvede24 <cdaniela.vd@gmail.com>
2024-09-09 23:18:44 +0200

    events bd ok

--- frontend/src/app/services/event.service.ts
+++ frontend/src/app/services/event.service.ts
@@ -4,32 +4,26 @@ import { Observable } from 'rxjs';
 import { CalendarEvent } from '../interfaces/event.interface';  // Cambia por la ruta correcta
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class EventService {
-    private apiUrl = 'http://localhost:3000/events';  // Asegúrate de que coincida con tu endpoint en backend
+  private apiUrl = 'http://localhost:3000/events';  // Asegúrate de que coincida con tu endpoint en backend
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    getEvents(): Observable<CalendarEvent[]> {
-        return this.http.get<CalendarEvent[]>(this.apiUrl);
-    }
+  getEvents(): Observable<CalendarEvent[]> {
+    return this.http.get<CalendarEvent[]>(this.apiUrl, { withCredentials: true });
+  }
 
-    createEvent(newEvent: CalendarEvent): Observable<CalendarEvent> {
-        return this.http.post<CalendarEvent>(`${this.apiUrl}/events`, newEvent);
-    }
+  createEvent(newEvent: CalendarEvent): Observable<CalendarEvent> {
+    return this.http.post<CalendarEvent>(`${this.apiUrl}`, newEvent, { withCredentials: true });
+  }
 
+  updateEvent(id: number, updatedEvent: CalendarEvent): Observable<CalendarEvent> {
+    return this.http.put<CalendarEvent>(`${this.apiUrl}/${id}`, updatedEvent, { withCredentials: true });
+  }
 
-    addEvent(event: CalendarEvent): Observable<CalendarEvent> {
-        return this.http.post<CalendarEvent>(this.apiUrl, event);
-    }
-
-    updateEvent(id: number, updatedEvent: CalendarEvent): Observable<CalendarEvent> {
-        return this.http.put<CalendarEvent>(`${this.apiUrl}/events/${id}`, updatedEvent);
-      }
-
-    deleteEvent(id: number): Observable<void> {
-        return this.http.delete<void>(`${this.apiUrl}/${id}`);
-    }
+  deleteEvent(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, { withCredentials: true });
+  }
 }
-
